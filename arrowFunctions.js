7146a08e609fa13d42cc4e5541c7d9da0d40935e
Caroline Knote <cj.knote@gmail.com>
2021-12-20 17:04:37 -0800

    adding solution

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -5,3 +5,15 @@
 //   }
 
 const double = arr => arr.map(val => val *2);
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+const squareEvens = numbers => numbers.map(num => num**2).filter(square => square % 2 === 0);
